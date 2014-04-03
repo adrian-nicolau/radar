7c1b51751a70db4561eb8f8433d3ea2766bd3feb
Adrian Nicolau <adrian.nicolau74@gmail.com>
2014-04-03 19:19:17 +0300

    Add script to generate signal map; add map for single AP

---
+++ python/contour.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.interpolate
+from operator import itemgetter
+
+import parser
+import plotter
+
+# Establish data
+
+macEnd = raw_input("Please enter last byte of MAC address: ")
+
+x = np.array([el[0] for el in plotter.data])
+y = np.array([el[1] for el in plotter.data])
+z = np.array(plotter.getRSSIs(parser.FSLMARK + ":" + macEnd))
+
+# Set up a regular grid of interpolation points
+xi, yi = np.linspace(x.min(), x.max(), 100), np.linspace(y.min(), y.max(), 100)
+xi, yi = np.meshgrid(xi, yi)
+
+# Interpolate
+rbf = scipy.interpolate.Rbf(x, y, z, function='linear')
+zi = rbf(xi, yi)
+
+plt.imshow(zi, vmin=z.min(), vmax=z.max(), origin='lower', 
+            extent=[x.min(), x.max(), y.min(), y.max()])
+plt.scatter(x, y, c=z)
+plt.colorbar()
+plt.show()
--- python/plotter.py
+++ python/plotter.py
@@ -13,20 +13,21 @@ def getRSSIs(bssid):
                 RSSIs.append(el[1])
     return RSSIs
 
-allRSSIs = [getRSSIs(bssid) for bssid in parser.bssids if bssid.startswith(parser.FSLMARK)]
-Ys = [el[1] for el in data]
+if __name__ == "__main__":
+    allRSSIs = [getRSSIs(bssid) for bssid in parser.bssids if bssid.startswith(parser.FSLMARK)]
+    Ys = [el[1] for el in data]
 
-i = 0
-for values in allRSSIs:
-    print values
-    print Ys
-    label = parser.bssids.pop()
-    plt.plot(Ys, values, label=label)
-    i += 1
+    i = 0
+    for values in allRSSIs:
+        print values
+        print Ys
+        label = parser.bssids.pop()
+        plt.plot(Ys, values, label=label)
+        i += 1
 
-plt.xlabel('Y')
-plt.ylabel('RSSI')
-plt.title('Signal strength')
-plt.legend()
-plt.gca().invert_xaxis()
-plt.show()
+    plt.xlabel('Y')
+    plt.ylabel('RSSI')
+    plt.title('Signal strength')
+    plt.legend()
+    plt.gca().invert_xaxis()
+    plt.show()
