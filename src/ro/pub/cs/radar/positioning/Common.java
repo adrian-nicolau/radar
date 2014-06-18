13e82de5fb22260fceb0e7da4480b57741136512
Adrian Nicolau <adrian.nicolau74@gmail.com>
2014-06-18 18:24:32 +0300

    Non visible APs contribute to distance w/ 42 dBm

--- src/ro/pub/cs/radar/positioning/Common.java
+++ src/ro/pub/cs/radar/positioning/Common.java
@@ -3,27 +3,31 @@ package ro.pub.cs.radar.positioning;
 import java.util.HashMap;
 import java.util.Map;
 
-import ro.pub.cs.radar.Constants;
-
 public class Common {
 
-	public static double euclideanDistance(HashMap<String, Double> a, HashMap<String, Integer> b) {
+	public static double euclideanDistance(HashMap<String, Double> off, HashMap<String, Integer> on) {
 		double distance = 0;
-		for (Map.Entry<String, Double> e1 : a.entrySet()) {
-			for (Map.Entry<String, Integer> e2 : b.entrySet()) {
+		boolean found = false;
+		for (Map.Entry<String, Integer> e1 : on.entrySet()) {
+			for (Map.Entry<String, Double> e2 : off.entrySet()) {
 				if (e1.getKey().equals(e2.getKey())) {
-					distance += Math.pow(e1.getValue() - e2.getValue(), 2);
+					distance += Math.pow(((double) (e1.getValue())) - e2.getValue(), 2);
+					found = true;
 				}
+			}
+			if (!found) {
+				distance += 42; // the answer to everything
+				found = false;
 			}
 		}
 		return Math.sqrt(distance);
 	}
 
-	public static double calculateDistance(double levelInDb, double freqInMHz) {
-		double factor = 10.0 * Constants.n;
-		double exp = ((-1) * Constants.metersMHz * factor / 20.0 - (factor * Math.log10(freqInMHz)) + Math
-				.abs(levelInDb)) / factor;
-		return Math.pow(10.0, exp);
-	}
+	/*
+	 * public static double calculateDistance(double levelInDb, double
+	 * freqInMHz) { double factor = 10.0 * Constants.n; double exp = ((-1) *
+	 * Constants.metersMHz * factor / 20.0 - (factor * Math.log10(freqInMHz)) +
+	 * Math .abs(levelInDb)) / factor; return Math.pow(10.0, exp); }
+	 */
 
 }
