0c9bbaa99d08e0d4ffa87596f6cdbb98e8a89c6a
Adrian Nicolau <adrian.nicolau74@gmail.com>
2014-06-26 18:42:49 +0300

    Add trilateration image

--- python/constants.py
+++ python/constants.py
@@ -5,19 +5,20 @@ NETGEAR = "00:90:4c:7e:00:6e"
 
 x42, y42 = 612, 1063
 xCISCO, yCISCO = 529, 417
-xNETGEAR, yNETGEAR = 785, 842
 xDLINK, yDLINK = 465, 1345
+xNETGEAR, yNETGEAR = 785, 842
 
 # 3.27
-#xME, yME = 800, 906
-#rssi42 = -49
-#rssiCISCO = -66
-#rssiNETGEAR = -39
-#rssiDLINK = -60
+xME, yME = 800, 906
+rssi42 = -49
+rssiCISCO = -66
+rssiDLINK = -60
+rssiNETGEAR = -39
 
 # 3.8
-xME, yME = 583, 457
-rssi42 = -48
-rssiCISCO = -43
-rssiNETGEAR = -52
-rssiDLINK = -67
+#xME, yME = 583, 457
+#rssi42 = -48
+#rssiCISCO = -43
+#rssiDLINK = -67
+#rssiNETGEAR = -52
+
--- python/trilateration.py
+++ python/trilateration.py
@@ -33,10 +33,10 @@ fig.gca().add_artist(plt.Circle((constants.xDLINK, constants.yDLINK), 3, color='
 fig.gca().add_artist(plt.Circle((constants.xNETGEAR, constants.yNETGEAR), 3, color='black'))
 
 # draw circles - get radii from ./fit.py
-drawCircle(constants.x42, constants.y42, 252, 'r')
-drawCircle(constants.xCISCO, constants.yCISCO, 138, 'b')
-drawCircle(constants.xDLINK, constants.yDLINK, 769, 'gray')
-drawCircle(constants.xNETGEAR, constants.yNETGEAR, 365, 'g')
+drawCircle(constants.x42, constants.y42, 272, 'r')
+drawCircle(constants.xCISCO, constants.yCISCO, 1128, 'b')
+drawCircle(constants.xDLINK, constants.yDLINK, 473, 'gray')
+drawCircle(constants.xNETGEAR, constants.yNETGEAR, 134, 'g')
 
 plt.gca().invert_yaxis()
 
