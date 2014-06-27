006ae09c4e057ffd2fa118b905de2a37c4ab84b2
Adrian Nicolau <adrian.nicolau74@gmail.com>
2014-06-27 16:35:07 +0300

    Add measurements for trilateration input

--- python/constants.py
+++ python/constants.py
@@ -15,10 +15,66 @@ rssiCISCO = -66
 rssiDLINK = -60
 rssiNETGEAR = -39
 
-# 3.8
+# 3.8 Marius
 #xME, yME = 583, 457
 #rssi42 = -48
 #rssiCISCO = -43
 #rssiDLINK = -67
 #rssiNETGEAR = -52
+
+# 3.22 Flavius
+#xME, yME = 687, 671
+#rssi42 = -59
+#rssiCISCO = -62
+#rssiDLINK = 0
+#rssiNETGEAR = -36
+
+# 3.10 Alexandra
+#xME, yME = 680, 437
+#rssi42 = -63
+#rssiCISCO = -39
+#rssiDLINK = 0
+#rssiNETGEAR = -50
+
+# 3.14 George
+#xME, yME = 853, 480
+#rssi42 = -63
+#rssiCISCO = -49
+#rssiDLINK = 0
+#rssiNETGEAR = -49
+
+# 3.21 Mihai
+#xME, yME = 827, 677
+#rssi42 = -54
+#rssiCISCO = -57
+#rssiDLINK = 0
+#rssiNETGEAR = -42
+
+# 3.28 Daniel
+#xME, yME = 664, 893
+#rssi42 = -47
+#rssiCISCO = -59
+#rssiDLINK = 0
+#rssiNETGEAR = -51
+
+# 3.6 Elvis
+#xME, yME = 501, 453
+#rssi42 = -64
+#rssiCISCO = -41
+#rssiDLINK = 0
+#rssiNETGEAR = -55
+
+# 3.19 Florin
+#xME, yME = 677, 538
+#rssi42 = -64
+#rssiCISCO = -48
+#rssiDLINK = 0
+#rssiNETGEAR = -52
+
+# 3.12 Dragos M
+#xME, yME = 774, 446
+#rssi42 = -65
+#rssiCISCO = -47
+#rssiDLINK = 0
+#rssiNETGEAR = -50
 
